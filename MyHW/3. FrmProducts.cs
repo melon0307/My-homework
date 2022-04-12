fb5c82ad47ca257ff7b99c63d9e0ad9ea9d2240d
Michael Lee <melon0307@hotmail.com>
2022-04-12 23:19:03 +0800

    ver 1.7 Finished

--- MyHW/3. FrmProducts.cs
+++ MyHW/3. FrmProducts.cs
@@ -17,17 +17,16 @@ namespace MyHomeWork
             InitializeComponent();
             this.productsTableAdapter1.Fill(this.nwDataSet1.Products);
             Output();
-            lblResult.Text = $"結果: {this.bindingSource1.Count} 筆";
+            lblResult.Text = $"結果: 共 {this.bindingSource1.Count} 筆";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             try
             {
-                this.productsTableAdapter1.UnitPriceBetween(this.nwDataSet1.Products, int.Parse(textBox1.Text), int.Parse(textBox2.Text));
-                this.dataGridView1.DataSource = this.nwDataSet1.Products;
+                this.productsTableAdapter1.UnitPriceBetween(this.nwDataSet1.Products, int.Parse(textBox1.Text), int.Parse(textBox2.Text));                
                 Output();
-                this.lblResult.Text = $"結果:  {this.textBox1.Text} 元 到 {this.textBox2.Text} 元, 共 {this.bindingSource1.Count} 筆";
+                this.lblResult.Text = $"結果: 單價介於 {this.textBox1.Text} 元到 {this.textBox2.Text} 元, 共 {this.bindingSource1.Count} 筆";
             }
             catch(Exception ex)
             {
@@ -44,10 +43,10 @@ namespace MyHomeWork
             }
             else
             {
-                this.productsTableAdapter1.ProductName(this.nwDataSet1.Products, "%" + textBox3.Text + "%");
-                this.dataGridView1.DataSource = this.nwDataSet1.Products;
+                string keyWord = $"%{textBox3.Text}%";
+                this.productsTableAdapter1.ProductName(this.nwDataSet1.Products, keyWord);                
                 Output();
-                this.lblResult.Text = $"結果: 名字內有 {textBox3.Text} 的, 共 {this.bindingSource1.Count} 筆";
+                this.lblResult.Text = $"結果: 名字內有 {textBox3.Text} 的產品, 共 {this.bindingSource1.Count} 筆";
             }
         }
 
@@ -58,22 +57,22 @@ namespace MyHomeWork
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.bindingSource1.Position = 0;            
+            this.bindingSource1.MoveFirst() ;            
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            this.bindingSource1.Position -= 1;            
+            this.bindingSource1.MovePrevious();            
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-            this.bindingSource1.Position += 1;
+            this.bindingSource1.MoveNext(); ;
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            this.bindingSource1.Position = this.bindingSource1.Count;            
+            this.bindingSource1.MoveLast();            
         }
 
         private void Output()
--- MyHW/Program.cs
+++ MyHW/Program.cs
@@ -19,7 +19,7 @@ namespace MyHW
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCategoryProducts());
+            Application.Run(new FrmProducts());
         }
     }
 }
