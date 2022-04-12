e569e158aa7587d332424cc24bd52ff230a46e44
Michael Lee <melon0307@hotmail.com>
2022-04-12 16:01:51 +0800

    ver 1.1 finished

--- MyHW/3. FrmProducts.cs
+++ MyHW/3. FrmProducts.cs
@@ -16,48 +16,59 @@ namespace MyHomeWork
         {
             InitializeComponent();
             this.productsTableAdapter1.Fill(this.nwDataSet1.Products);
-            this.bindingSource1.DataSource = this.nwDataSet1.Products;
-            this.dataGridView1.DataSource = this.bindingSource1;
-            this.bindingNavigator1.BindingSource = this.bindingSource1;
+            Output();
+            lblResult.Text = $"結果: {this.bindingSource1.Count} 筆";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.productsTableAdapter1.UnitPriceBetween(this.nwDataSet1.Products, int.Parse(textBox1.Text), int.Parse(textBox2.Text));
             this.dataGridView1.DataSource = this.nwDataSet1.Products;
-            this.lblResult.Text = $"結果  {this.textBox1.Text} 元 到 {this.textBox2.Text} 元, 共 {this.bindingSource1.Count}   筆";
+            this.lblResult.Text = $"結果:  {this.textBox1.Text} 元 到 {this.textBox2.Text} 元, 共 {this.bindingSource1.Count}   筆";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             this.productsTableAdapter1.ProductName(this.nwDataSet1.Products, "%"+textBox3.Text+"%");
             this.dataGridView1.DataSource = this.nwDataSet1.Products;
+            this.lblResult.Text = $"結果: 名字內有 {textBox3.Text} 的, 共 {this.bindingSource1.Count}   筆";
         }
 
         private void bindingSource1_CurrentChanged(object sender, EventArgs e)
-        {
-            this.lblResult.Text = $"結果   {this.bindingSource1.Count}   筆";
+        {            
             this.label2.Text = $"{this.bindingSource1.Position + 1} / {this.bindingSource1.Count}";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             this.bindingSource1.Position = 0;
+            Output();
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
             this.bindingSource1.Position -= 1;
+            Output();
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
             this.bindingSource1.Position += 1;
+            Output();
+
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
             this.bindingSource1.Position = this.bindingSource1.Count;
+            Output();
+        }
+
+        private void Output()
+        {
+            this.bindingSource1.DataSource = this.nwDataSet1.Products;
+            this.dataGridView1.DataSource = this.bindingSource1;
+            this.bindingNavigator1.BindingSource = this.bindingSource1;
         }
     }
 }
