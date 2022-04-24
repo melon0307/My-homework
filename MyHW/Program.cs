aea5f7c2fd5724319d17c49e3fddc558c4bca250
Michael Lee <melon0307@hotmail.com>
2022-04-24 21:03:13 +0800

    4.24

--- MyHW/Program.cs
+++ MyHW/Program.cs
@@ -19,7 +19,7 @@ namespace MyHW
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmTreeView());
+            Application.Run(new FrmLogon());
         }
     }
 }
